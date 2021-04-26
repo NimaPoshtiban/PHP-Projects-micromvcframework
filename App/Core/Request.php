947bc8c45c1cdaa1a274049be87e9eb2bdbd00ad
Nima Poshtiban <poshtibannima@gmail.com>
2021-04-26 17:10:42 +0430

    autoload now obeys PSR4 standard

---
+++ App/Core/Request.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Core;
+
+class Request
+{
+  public function __constructor()
+  {
+    echo "Request class";
+  }
+}
--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 # font controller
 
+use App\Core\Request;
+
+include "vendor/autoload.php";
+
 $_SERVER['REQUEST_URI'];
+
+new App\Core\Request();
